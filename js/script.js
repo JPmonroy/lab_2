16301449b7587408f90e805bafc18b8098ea5219
JPmonroy <69050030+JPmonroy@users.noreply.github.com>
2021-01-18 19:45:34 -0600

    Se subió repositorio
    Se adjunto archivos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
+var saludo = "hola JavaScript"
+var edad = 26
+var unPoco = 1.5
+var siNo = true
+var seccion = "a"
+
+//MOSTRAR DATOS EN CONSOLA
+console.log(saludo)
+
+var datoApellido = document.getElementById("favorito")
